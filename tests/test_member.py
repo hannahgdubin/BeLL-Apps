4d218d8d2ea940bb82f7f00bddca6a52aa3e8d68
Antonella Bernobich Dean <aberdean@users.noreply.github.com>
2016-12-20 09:48:27 +0100

    should work, needs refactoring

--- tests/test_member.py
+++ tests/test_member.py
@@ -14,6 +14,8 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
 
 from time import sleep
+from random import choice
+from string import ascii_lowercase
 
 # NOTICE: This code is totally ugly and needs refactoring, but I'm 
 #         just testing functionality before today's meeting.
@@ -24,6 +26,8 @@ class MemberTest(BaseCase):
     def test_add_member(self):
         driver = self.driver
         
+        # random string to fill all fields
+        fill = "a"
         # data = birthdate, gender, level
         data = [["1", "11", "1924"], "Female", "4"]
         
@@ -31,24 +35,26 @@ class MemberTest(BaseCase):
         # get to the form page
         self.setup()
         # fill up the form without uploading a file
-        self.fill_form("c", data, True)
+        self.fill_form(fill, data)
         #TODO: test new member login
         
         # adding already existing member
         # get back to the form page
         self.setup()
         # fill up the form without uploading a file
-        self.fill_form("c", data)
+        self.fill_form(fill, data, False)
         #TODO: test new member login
 
 #        # adding new member and uploading file
+#        # random string to fill all fields
+#        fill = "b"
 #        # data = birthdate, gender, level
 #        data = [["31", "0", "1934"], "Male", "Higher"]
 #        # attachment = "" #TODO: insert filename
 #        # get to the form page
 #        self.setup()
 #        # fill up the form uploading a file
-#        self.fill_form("b", data, True, True, attachment)
+#        self.fill_form(fill, data, True, True, attachment)
         
         
     def setup(self):
@@ -69,7 +75,7 @@ class MemberTest(BaseCase):
         expected = bell.get_url() + "#member/add"
         self.assertEqual(actual, expected)
     
-    def fill_form(self, fill, data, new, upload=False, attachment=None):
+    def fill_form(self, fill, data, new=True, upload=False, attachment=None):
         """ (string, list, bool, bool, string) -> string
         
         Helper function to fill out a new member's registration form.
@@ -91,6 +97,7 @@ class MemberTest(BaseCase):
         fields = ["firstName", "lastName", "middleNames", "login", "password", "phone", "email", "language"]
         for field in fields:
             elem = driver.find_element_by_name(field)
+            elem.clear()
             elem.send_keys(fill)
         
         birthdate, gender, level = data
@@ -132,9 +139,13 @@ class MemberTest(BaseCase):
             expected = "Login already exists."
         self.assertEqual(actual, expected)
         Alert(driver).accept()
+        # if the user already exists, then add a new user
+        if not new:
+            fill = "".join(choice(ascii_lowercase) for i in range(3))
+            self.fill_form(fill, data, True)
         # test we're back to the login page
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "login")))
-        sleep(5)
+        sleep(10)
         actual = driver.current_url
         expected = bell.get_url() + "#login"
         self.assertEqual(actual, expected)
